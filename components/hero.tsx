31e6399fa67a6815e60efb2e80f5fa8c5da9a674
Avir4m <avir4m@gmail.com>
2024-12-02 13:41:54 +0200

    updated hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,17 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
+      <h1 className="text-3xl">Welcome to the best app in the world</h1>
+
+      <Button asChild>
+        <Link href="/sign-up">
+        Get Started
+        </Link>
+      </Button>
+
     </div>
   );
 }
