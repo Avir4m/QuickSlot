58b73ac1200f4ba66fc91d2ed895db9c65dc9ffe
Avir4m <avir4m@gmail.com>
2024-12-26 11:40:21 +0200

    added org settings page

--- app/(protected)/organizations/[uuid]/page.tsx
+++ app/(protected)/organizations/[uuid]/page.tsx
@@ -1,6 +1,7 @@
 import { getOrganization, isOwnerOfOrg } from "@/app/actions/organizations";
-import { Button } from "@/components/ui/button";
-
+import { Settings } from "lucide-react";
+import Link from "next/link";
+ 
 export default async function organizationPage({ params }: {params: Promise<{ uuid: string }>}) {
     const uuid = (await params).uuid;
     const organization = await getOrganization(uuid);
@@ -8,12 +9,16 @@ export default async function organizationPage({ params }: {params: Promise<{ uu
 
     return (
         <>
-            { organization.name }
-            {ownerCheck && (
-                <Button className="bg-red-500 text-white">
-                    Delete Organization
-                </Button>
-            )}
+            <div className="flex gap-32">
+                { organization.name }
+                {ownerCheck && (
+                <Link href={`/organizations/${uuid}/settings`}>
+                    <Settings/>
+                </Link>
+                )}
+            </div>
+            
+            
         </>
     );
 }
